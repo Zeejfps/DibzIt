420d95878227375d341062017ad10c0731a465d1
zeejfps <zeejfps@gmail.com>
2017-10-08 10:40:13 -0500

    Add project files.

---
+++ DibzIt/DibzIt/Pages/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DibzIt.Pages;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DibzIt {
+    public partial class MainPage : ContentPage {
+
+        public DateTime MinimumDate { get; set; }
+        public DateTime MaximumDate { get; set; }
+
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing() {
+            base.OnAppearing();
+            DatePicker.MinimumDate = DateTime.Now;
+            DatePicker.MaximumDate = DatePicker.MinimumDate.AddDays(6);
+        }
+
+        async void OnSearchButtonTapped(object sender, EventArgs e) {
+
+            DateTime selectedDate = DatePicker.Date;
+            if (selectedDate == DateTime.Now.Date) {
+                selectedDate = DateTime.Now;
+            }
+            int duration = DurationPicker.SelectedIndex + 1;
+            await Navigation.PushAsync(new TimeSlotsPage(selectedDate, duration));
+        }
+    }
+}
---
+++ DibzIt/DibzIt/Pages/TimeSlotsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DibzIt.Pages {
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TimeSlotsPage : ContentPage {
+
+        List<string> timeSlots = new List<string>();
+
+        public TimeSlotsPage(DateTime startDate, int duration) {
+            InitializeComponent();
+            DateTime fromDateTime = startDate;
+            for (int i = startDate.Hour; i < 24; i++) {
+                string from = fromDateTime.ToString("hh:00 tt");
+                DateTime toDateTime = fromDateTime.AddHours(duration);
+                string to = toDateTime.ToString("hh:00 tt");
+                timeSlots.Add(from + " - " + to);
+                fromDateTime = fromDateTime.AddHours(1);
+            }
+            TimeSlotsList.ItemsSource = timeSlots;
+        }
+
+    }
+}
